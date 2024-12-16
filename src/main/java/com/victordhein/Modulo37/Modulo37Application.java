a14c1fae0eab5e436054704ed994effcefae898c
VictorDhein <victordhein@gmail.com>
2024-12-16 08:15:43 -0300

    Initial commit

---
+++ src/main/java/com/victordhein/Modulo37/Modulo37Application.java
@@ -0,0 +1,13 @@
+package com.victordhein.Modulo37;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Modulo37Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Modulo37Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/victordhein/Modulo37/Modulo37ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.victordhein.Modulo37;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Modulo37ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
